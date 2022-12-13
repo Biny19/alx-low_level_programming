d5b9c3338f94a9b6ba1072476838ada055ff4e60
Biny19 <binalfew.tadele@gmail.com>
2022-12-13 00:58:05 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Entry point
+* Return: 0
+*/
+void print_alphabet_x10(void)
+{
+char c;
+for (c = 1; c <= 10; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
