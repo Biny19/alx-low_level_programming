a9c3aeeeb9bfb47e42a3d123eaa0f0c588d74b9d
Binalfew <99319057+Biny19@users.noreply.github.com>
2022-12-23 09:38:37 +0300

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,11 +5,7 @@ int main(void)
 int n;
 int a[5];
 int *p;
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> b0281a8720206f860ac3c217ed785798f5fa88d7
 a[2] = 1024;
 p = &n;
 /*
@@ -20,11 +16,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-<<<<<<< HEAD
-*(p + 5) =98;
-=======
-  *(p + 5) = 98;
->>>>>>> b0281a8720206f860ac3c217ed785798f5fa88d7
+*(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
