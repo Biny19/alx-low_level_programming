439d82cf40a48e003d648ae645b3cf96924edb13
Biny19 <binalfew.tadele@gmail.com>
2023-03-16 04:35:01 -0700

    It's all about the 0x17. C - Doubly linked lists project in C

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,43 +1,43 @@
 #include "lists.h"
 /**
-* delete_dnodeint_at_index - deletes a node
-* in a dlinked list at a certain index
-* @head: pointer to the first element in the list
-* @index: index of the node to delete
-*
-* Return: 1 (Success), or -1 (Fail)
-*/
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
+ * @head: head of the list
+ * @index: index of the new node
+ * 
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *temp;
-dlistint_t *current;
+dlistint_t *h1;
+dlistint_t *h2;
 unsigned int i;
-temp = *head;
-if (temp != NULL)
-while (temp->prev != NULL)
-temp = temp->prev;
+h1 = *head;
+if (h1 != NULL)
+while (h1->prev != NULL)
+h1 = h1->prev;
 i = 0;
-while (temp != NULL)
+while (h1 != NULL)
 {
 if (i == index)
 {
 if (i == 0)
 {
-*head = temp->next;
+*head = h1->next;
 if (*head != NULL)
 (*head)->prev = NULL;
 }
 else
 {
-current->next = temp->next;
-if (temp->next != NULL)
-h1->next->prev = current;
+h2->next = h1->next;
+if (h1->next != NULL)
+h1->next->prev = h2;
 }
-free(temp);
+free(h1);
 return (1);
 }
-current = temp;
-temp = temp->next;
+h2 = h1;
+h1 = h1->next;
 i++;
 }
 return (-1);
