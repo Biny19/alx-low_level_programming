a47c963686ab13d316166d7ddb3369b8a1f68d0b
Biny19 <binalfew.tadele@gmail.com>
2023-03-20 18:11:03 -0700

    try

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp https://github.com/Biny19/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -O /tmp https://github.com/Biny19/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lottery.so
 export LD_PRELOAD=/tmp/lottery.so
