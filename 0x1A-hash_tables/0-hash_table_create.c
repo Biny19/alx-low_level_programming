bac4661780b41fb1c9ba104f32a47b6fa22934b4
Biny19 <binalfew.tadele@gmail.com>
2023-04-07 05:04:20 -0700

    It's all about 0x1A. C - Hash tables project in C

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 
 	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
+	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) *);
